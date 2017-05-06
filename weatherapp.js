d9202be3dd9587d5d2d77ccc5ed5b005aa07e620
ryoboua <ryoboua@gmail.com>
2017-05-05 22:57:00 -0400

    Started appending weather info to DOM

--- weatherapp.js
+++ weatherapp.js
@@ -55,17 +55,41 @@ function weatherResult (weather){
 console.log(weather);
 var cityName = weather.city.name;
 var countryName = weather.city.country;
+var forecastInfo = []
 
-for(i=0; i<7;i++){
-  var day = {
-    dayHigh: weather.list[i].temp.max,
-    dayLow: weather.list[i].temp.min,
-    description: weather.list[i].weather[0].description,
-    icon: weather.list[i].weather[0].icon,
-    pressure: weather.list[i].pressure,
-    humidity: weather.list[i].humidity
+  for(i=0; i<7;i++){
+    var day = {
+      dayHigh: weather.list[i].temp.max,
+      dayLow: weather.list[i].temp.min,
+      description: weather.list[i].weather[0].description,
+      icon: weather.list[i].weather[0].icon,
+      pressure: weather.list[i].pressure,
+      humidity: weather.list[i].humidity
+    }
+    forecastInfo.push(day);
   }
-console.log(day)
+console.log(forecastInfo)
+showForecast(cityName,countryName,forecastInfo)
 }
 
+function showForecast(city,country,forecast){
+  var $cityName = $('<h2>').appendTo('.city_info').text(city +','+ country);
+
+  forecast.forEach(function(cast){
+    var $day = $('<li>').addClass('day_weather').appendTo('.forecast_list')
+
+    var $date = $('<p>').appendTo($day).text('Date goes here')
+
+    var $image = $('<img>').attr('src','http://openweathermap.org/img/w/'+ cast.icon +'.png').appendTo($day);
+
+    var $tempWrap = $('<div>').addClass('temp_wrap').appendTo($day)
+    var $high = $('<p>').addClass('high').appendTo($tempWrap).text(cast.dayHigh)
+    var $low = $('<p>').addClass('low').appendTo($tempWrap).text(cast.dayLow)
+    var $description = $('<p>').addClass('weather_description').appendTo($tempWrap).text(cast.description)
+
+    var $atmosphere = $('<div>').addClass('atmosphere').appendTo($day)
+    var $pressure = $('<p>').addClass('atmosphere_text').appendTo($atmosphere).text(cast.pressure)
+    var $humidity = $('<p>').addClass('atmosphere_text').appendTo($atmosphere).text(cast.humidity +'%')
+
+  })
 }
